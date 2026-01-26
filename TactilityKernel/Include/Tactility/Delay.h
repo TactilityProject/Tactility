c3ecc9553036b1230f4095526d73ff5cc2f80539
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2026-01-26 21:30:43 +0100

    Added Time.h and Delay.h

---
+++ TactilityKernel/Include/Tactility/Delay.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "Time.h"
+#include <stdint.h>
+
+#ifdef ESP_PLATFORM
+#include <rom/ets_sys.h>
+#else
+#include <unistd.h>
+#endif
+
+#include <assert.h>
+#include "Tactility/FreeRTOS/FreeRTOS.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Delay the current task for the specified amount of ticks
+ * @warning Does not work in ISR context
+ */
+static inline void delay_ticks(TickType_t ticks) {
+    assert(xPortInIsrContext() == pdFALSE);
+    if (ticks == 0U) {
+        taskYIELD();
+    } else {
+        vTaskDelay(ticks);
+    }
+}
+
+/**
+ * Delay the current task for the specified amount of milliseconds
+ * @warning Does not work in ISR context
+ */
+static inline void delay_millis(uint32_t milliSeconds) {
+    delay_ticks(millis_to_ticks(milliSeconds));
+}
+
+/**
+ * Stall the currently active CPU core for the specified amount of microseconds.
+ * This does not allow other tasks to run on the stalled CPU core.
+ */
+static inline void delay_micros(uint32_t microseconds) {
+#ifdef ESP_PLATFORM
+    ets_delay_us(microseconds);
+#else
+    usleep(microseconds);
+#endif
+}
+
+#ifdef __cplusplus
+}
+#endif
---
+++ Tests/TactilityKernel/TimeAndDelay.cpp
@@ -0,0 +1,27 @@
+#include "doctest.h"
+#include <Tactility/Time.h>
+#include <Tactility/Delay.h>
+
+TEST_CASE("delay ticks should be accurate") {
+    auto start_time = get_ticks();
+    delay_ticks(100);
+    auto end_time = get_ticks();
+    auto difference = end_time - start_time;
+    CHECK_EQ(difference == 100, true);
+}
+
+TEST_CASE("delay millis should be accurate") {
+    auto start_time = get_millis();
+    delay_millis(100);
+    auto end_time = get_millis();
+    auto difference = end_time - start_time;
+    CHECK_EQ(difference == 100, true);
+}
+
+TEST_CASE("microsecond time should be accurate within 1 tick") {
+    auto start_time = get_micros_since_boot();
+    delay_millis(100);
+    auto end_time = get_micros_since_boot();
+    auto difference = (end_time - start_time) / 1000;
+    CHECK_EQ(difference >= 99 && difference <= 101, true);
+}
