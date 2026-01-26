e22dd87775fd1ce3df2a20ad3b1590f38f16def5
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2026-01-26 22:51:24 +0100

    Improve SD card setup and fix WiFi

--- Devices/m5stack-tab5/Source/Configuration.cpp
+++ Devices/m5stack-tab5/Source/Configuration.cpp
@@ -122,5 +122,31 @@ extern const Configuration hardwareConfiguration = {
                 .clk_flags = 0
             }
         }
+    },
+    .spi = {
+        // SDCard
+        spi::Configuration {
+            .device = SPI3_HOST,
+            .dma = SPI_DMA_CH_AUTO,
+            .config = {
+                .mosi_io_num = GPIO_NUM_44,
+                .miso_io_num = GPIO_NUM_39,
+                .sclk_io_num = GPIO_NUM_43,
+                .quadwp_io_num = GPIO_NUM_NC,
+                .quadhd_io_num = GPIO_NUM_NC,
+                .data4_io_num = GPIO_NUM_NC,
+                .data5_io_num = GPIO_NUM_NC,
+                .data6_io_num = GPIO_NUM_NC,
+                .data7_io_num = GPIO_NUM_NC,
+                .data_io_default_level = false,
+                .max_transfer_sz = 0,
+                .flags = 0,
+                .isr_cpu_id = ESP_INTR_CPU_AFFINITY_AUTO,
+                .intr_flags = 0
+            },
+            .initMode = spi::InitMode::ByTactility,
+            .isMutable = false,
+            .lock = nullptr
+        }
     }
 };
--- Devices/m5stack-tab5/Source/devices/SdCard.h
+++ Devices/m5stack-tab5/Source/devices/SdCard.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <Tactility/hal/sdcard/SdCardDevice.h>
+#include <memory>
 
-// Create SD card device for jc1060p470ciwy using SDMMC slot 0 (4-bit)
-std::shared_ptr<tt::hal::sdcard::SdCardDevice> createSdCard();
+using tt::hal::sdcard::SdCardDevice;
+
+std::shared_ptr<SdCardDevice> createSdCard();
