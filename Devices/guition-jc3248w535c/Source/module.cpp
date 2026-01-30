37b6fe7e98ec4dfcb4ab74f28a7f3ebb1d6c04ae
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2026-01-31 00:03:53 +0100

    Update new device with module.cpp

---
+++ Devices/guition-jc3248w535c/Source/module.cpp
@@ -0,0 +1,22 @@
+#include <tactility/module.h>
+
+extern "C" {
+
+static error_t start() {
+    // Empty for now
+    return ERROR_NONE;
+}
+
+static error_t stop() {
+    // Empty for now
+    return ERROR_NONE;
+}
+
+/** @warning The variable name must be exactly "device_module" */
+struct Module device_module = {
+    .name = "Module",
+    .start = start,
+    .stop = stop
+};
+
+}
