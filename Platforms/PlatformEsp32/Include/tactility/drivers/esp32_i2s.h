f491a86064166d95b2e14058382bae06b7883ae7
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2026-02-10 21:26:26 +0100

    Update pin type

--- Platforms/PlatformEsp32/Include/tactility/drivers/esp32_i2s.h
+++ Platforms/PlatformEsp32/Include/tactility/drivers/esp32_i2s.h
@@ -2,6 +2,7 @@
 #pragma once
 
 #include <tactility/drivers/i2s_controller.h>
+#include <tactility/drivers/gpio.h>
 #include <driver/i2s_common.h>
 
 #ifdef __cplusplus
@@ -10,11 +11,11 @@ extern "C" {
 
 struct Esp32I2sConfig {
     i2s_port_t port;
-    int pin_bclk;
-    int pin_ws;
-    int pin_data_out;
-    int pin_data_in;
-    int pin_mclk;
+    gpio_pin_t pin_bclk;
+    gpio_pin_t pin_ws;
+    gpio_pin_t pin_data_out;
+    gpio_pin_t pin_data_in;
+    gpio_pin_t pin_mclk;
 };
 
 #ifdef __cplusplus
