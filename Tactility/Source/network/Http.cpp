2df6cbb60a6a2e2edf3600eefb87d2c900b1a9c7
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2026-02-14 22:07:39 +0100

    Fix warnings

--- Tactility/Source/network/Http.cpp
+++ Tactility/Source/network/Http.cpp
@@ -35,16 +35,16 @@ void download(
         auto certificate_length = strlen(reinterpret_cast<const char*>(certificate.get())) + 1;
 
         // TODO: Fix for missing initializer warnings
-        auto config = std::make_unique<esp_http_client_config_t>(esp_http_client_config_t {
-            .url = url.c_str(),
-            .auth_type = HTTP_AUTH_TYPE_NONE,
-            .cert_pem = reinterpret_cast<const char*>(certificate.get()),
-            .cert_len = certificate_length,
-            .tls_version = ESP_HTTP_CLIENT_TLS_VER_TLS_1_3,
-            .method = HTTP_METHOD_GET,
-            .timeout_ms = 5000,
-            .transport_type = HTTP_TRANSPORT_OVER_SSL
-        });
+        auto config = std::make_unique<esp_http_client_config_t>();
+        memset(config.get(), 0, sizeof(esp_http_client_config_t));
+        config->url = url.c_str();
+        config->auth_type = HTTP_AUTH_TYPE_NONE;
+        config->cert_pem = reinterpret_cast<const char*>(certificate.get());
+        config->cert_len = certificate_length;
+        config->tls_version = ESP_HTTP_CLIENT_TLS_VER_TLS_1_3;
+        config->method = HTTP_METHOD_GET;
+        config->timeout_ms = 5000;
+        config->transport_type = HTTP_TRANSPORT_OVER_SSL;
 
         auto client = std::make_unique<EspHttpClient>();
         if (!client->init(std::move(config))) {
