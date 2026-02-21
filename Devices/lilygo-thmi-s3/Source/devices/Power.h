83a9e75f9bbda4b203d9ffd031a98495c6793fbe
Rivair Sabino dos Santos <65681783+Revers-BR@users.noreply.github.com>
2026-02-21 10:03:48 -0300

    Update Devices/lilygo-thmi-s3/Source/devices/Power.h
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- Devices/lilygo-thmi-s3/Source/devices/Power.h
+++ Devices/lilygo-thmi-s3/Source/devices/Power.h
@@ -4,7 +4,7 @@
 #include <Tactility/hal/power/PowerDevice.h>
 #include <driver/gpio.h>
 
-constexpr auto TDISPLAY_S3_POWEREN_GPIO = GPIO_NUM_10;
-constexpr auto TDISPLAY_S3_POWERON_GPIO = GPIO_NUM_14;
+constexpr auto THMI_S3_POWEREN_GPIO = GPIO_NUM_10;
+constexpr auto THMI_S3_POWERON_GPIO = GPIO_NUM_14;
 
 std::shared_ptr<tt::hal::power::PowerDevice> createPower();
