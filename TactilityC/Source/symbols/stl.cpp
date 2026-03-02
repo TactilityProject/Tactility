8c7558dc05da7dca7633ab51c32b1cbf8d5f9033
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2026-03-02 20:53:16 +0100

    Add new symbols

--- TactilityC/Source/symbols/stl.cpp
+++ TactilityC/Source/symbols/stl.cpp
@@ -5,6 +5,19 @@
 
 #include <bits/functexcept.h>
 
+#include <tactility/module.h>
+
+extern "C" {
+
+// std::map / std::set red-black tree non-template helpers.
+// We use the mangled names directly (same pattern as string.cpp) to avoid
+// ambiguity from the overloaded const/non-const variants in stl_tree.h.
+void* _ZSt18_Rb_tree_decrementPSt18_Rb_tree_node_base(void*);
+void* _ZSt18_Rb_tree_incrementPSt18_Rb_tree_node_base(void*);
+void  _ZSt29_Rb_tree_insert_and_rebalancebPSt18_Rb_tree_node_baseS0_RS_(bool, void*, void*, void*);
+
+}
+
 const esp_elfsym stl_symbols[] = {
     // Note: You have to use the mangled names here
     { "_ZSt17__throw_bad_allocv", (void*)&(std::__throw_bad_alloc) },
@@ -12,7 +25,11 @@ const esp_elfsym stl_symbols[] = {
     { "_ZSt25__throw_bad_function_callv", (void*)&(std::__throw_bad_function_call) },
     { "_ZSt20__throw_length_errorPKc", (void*)&(std::__throw_length_error) },
     { "_ZSt19__throw_logic_errorPKc", (void*)&std::__throw_logic_error },
-    // { "", (void*)&(std::) },
+    { "_ZSt24__throw_out_of_range_fmtPKcz", (void*)&std::__throw_out_of_range_fmt },
+    // std::map / std::set (red-black tree internals)
+    DEFINE_MODULE_SYMBOL(_ZSt18_Rb_tree_decrementPSt18_Rb_tree_node_base),
+    DEFINE_MODULE_SYMBOL(_ZSt18_Rb_tree_incrementPSt18_Rb_tree_node_base),
+    DEFINE_MODULE_SYMBOL(_ZSt29_Rb_tree_insert_and_rebalancebPSt18_Rb_tree_node_baseS0_RS_),
     // delimiter
     ESP_ELFSYM_END
 };
--- TactilityC/Source/tt_init.cpp
+++ TactilityC/Source/tt_init.cpp
@@ -33,6 +33,7 @@
 #include <cstring>
 #include <ctime>
 #include <ctype.h>
+#include <driver/ledc.h>
 #include <getopt.h>
 #include <dirent.h>
 #include <esp_log.h>
@@ -43,6 +44,7 @@
 #include <lwip/sockets.h>
 #include <lwip/netdb.h>
 #include <lwip/inet.h>
+#include <miniz.h>
 #include <sys/select.h>
 #include <locale.h>
 #include <setjmp.h>
@@ -220,6 +222,7 @@ const esp_elfsym main_symbols[] {
     ESP_ELFSYM_EXPORT(strtod),
     ESP_ELFSYM_EXPORT(strrchr),
     ESP_ELFSYM_EXPORT(strtol),
+    ESP_ELFSYM_EXPORT(strtoul),
     ESP_ELFSYM_EXPORT(strcspn),
     ESP_ELFSYM_EXPORT(strncat),
     ESP_ELFSYM_EXPORT(strpbrk),
@@ -392,6 +395,7 @@ const esp_elfsym main_symbols[] {
     ESP_ELFSYM_EXPORT(gpio_config),
     ESP_ELFSYM_EXPORT(gpio_get_level),
     ESP_ELFSYM_EXPORT(gpio_set_level),
+    ESP_ELFSYM_EXPORT(gpio_reset_pin),
     // driver/i2s_common.h
     ESP_ELFSYM_EXPORT(i2s_new_channel),
     ESP_ELFSYM_EXPORT(i2s_del_channel),
@@ -408,6 +412,17 @@ const esp_elfsym main_symbols[] {
     ESP_ELFSYM_EXPORT(i2s_channel_reconfig_std_clock),
     ESP_ELFSYM_EXPORT(i2s_channel_reconfig_std_slot),
     ESP_ELFSYM_EXPORT(i2s_channel_reconfig_std_gpio),
+    // miniz.h
+    ESP_ELFSYM_EXPORT(tinfl_decompress),
+    ESP_ELFSYM_EXPORT(tinfl_decompress_mem_to_callback),
+    ESP_ELFSYM_EXPORT(tinfl_decompress_mem_to_heap),
+    ESP_ELFSYM_EXPORT(tinfl_decompress_mem_to_mem),
+    // ledc
+    ESP_ELFSYM_EXPORT(ledc_update_duty),
+    ESP_ELFSYM_EXPORT(ledc_set_freq),
+    ESP_ELFSYM_EXPORT(ledc_channel_config),
+    ESP_ELFSYM_EXPORT(ledc_set_duty),
+    ESP_ELFSYM_EXPORT(ledc_timer_config),
     // delimiter
     ESP_ELFSYM_END
 };
