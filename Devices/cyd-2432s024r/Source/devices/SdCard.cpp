cdd2770326a3374990c3074080119120b0f7be4b
NellowTCS <neeljaiswal23@gmail.com>
2026-04-03 15:10:50 -0600

    Should be correct now, enabling ci to test

--- Devices/cyd-2432s024r/Source/devices/SdCard.cpp
+++ Devices/cyd-2432s024r/Source/devices/SdCard.cpp
@@ -6,18 +6,18 @@ using tt::hal::sdcard::SpiSdCardDevice;
 
 std::shared_ptr<SdCardDevice> createSdCard() {
     auto config = std::make_unique<SpiSdCardDevice::Config>(
-        GPIO_NUM_5,   // CS
+        GPIO_NUM_5,
         GPIO_NUM_NC,
         GPIO_NUM_NC,
         GPIO_NUM_NC,
         SdCardDevice::MountBehaviour::AtBoot,
         nullptr,
         std::vector<gpio_num_t>(),
-        SPI2_HOST     // shared bus
+        SPI3_HOST
     );
 
-    auto* spi_controller = device_find_by_name("spi0");
-    check(spi_controller, "spi0 not found");
+    auto* spi_controller = device_find_by_name("spi1");
+    check(spi_controller, "spi1 not found");
 
     return std::make_shared<SpiSdCardDevice>(std::move(config), spi_controller);
 }
