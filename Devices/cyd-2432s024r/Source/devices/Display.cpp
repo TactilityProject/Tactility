5441340dd6e1e85fa443e25985e740cb456cb5fb
NellowTCS <neeljaiswal23@gmail.com>
2026-04-04 01:26:43 -0600

    Modify display and touch configuration

--- Devices/cyd-2432s024r/Source/devices/Display.cpp
+++ Devices/cyd-2432s024r/Source/devices/Display.cpp
@@ -9,9 +9,9 @@ static std::shared_ptr<tt::hal::touch::TouchDevice> createTouch(esp_lcd_spi_bus_
         TOUCH_CS_PIN,
         LCD_HORIZONTAL_RESOLUTION,
         LCD_VERTICAL_RESOLUTION,
-        false,   // swapXY
-        true,   // mirrorX
-        false   // mirrorY
+        true,    // swapXY
+        false,   // mirrorX
+        false    // mirrorY
     );
     return std::make_shared<Xpt2046Touch>(std::move(configuration));
 }
@@ -31,8 +31,8 @@ std::shared_ptr<tt::hal::display::DisplayDevice> createDisplay() {
         .gapX = 0,
         .gapY = 0,
         .swapXY = true,
-        .mirrorX = true,
-        .mirrorY = false,
+        .mirrorX = false,
+        .mirrorY = true,
         .invertColor = false,
         .swapBytes = true,
         .bufferSize = LCD_BUFFER_SIZE,
