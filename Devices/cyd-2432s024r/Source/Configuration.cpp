2bcedeb85760bd4315e6270971597de0dd76f2ac
NellowTCS <neeljaiswal23@gmail.com>
2026-04-13 09:36:52 -0600

    Address review comments

--- Devices/cyd-2432s024r/Source/Configuration.cpp
+++ Devices/cyd-2432s024r/Source/Configuration.cpp
@@ -9,16 +9,6 @@
 using namespace tt::hal;
 
 static bool initBoot() {
-    // // Set the RGB LED Pins to output and turn them off
-    // ESP_ERROR_CHECK(gpio_set_direction(GPIO_NUM_27, GPIO_MODE_OUTPUT)); // Red
-    // ESP_ERROR_CHECK(gpio_set_direction(GPIO_NUM_12, GPIO_MODE_OUTPUT)); // Green
-    // ESP_ERROR_CHECK(gpio_set_direction(GPIO_NUM_13, GPIO_MODE_OUTPUT)); // Blue
-
-    // // 0 on, 1 off
-    // ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_27, 1)); // Red
-    // ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_12, 1)); // Green
-    // ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_13, 1)); // Blue
-
     return driver::pwmbacklight::init(LCD_PIN_BACKLIGHT);
 }
 
