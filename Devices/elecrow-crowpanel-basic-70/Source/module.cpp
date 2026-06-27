444c4452c80699e666b6fd703f79d164ed698ac1
Aleck59 <37000238+Aleck59@users.noreply.github.com>
2026-06-27 11:04:17 +0300

    Add elecrow-basic-70

---
+++ Devices/elecrow-crowpanel-basic-70/Source/module.cpp
@@ -0,0 +1,23 @@
+#include <tactility/module.h>
+
+extern "C" {
+
+static error_t start() {
+    // Empty for now
+    return ERROR_NONE;
+}
+
+static error_t stop() {
+    // Empty for now
+    return ERROR_NONE;
+}
+
+struct Module elecrow_crowpanel_basic_70_module = {
+    .name = "elecrow-crowpanel-basic-70",
+    .start = start,
+    .stop = stop,
+    .symbols = nullptr,
+    .internal = nullptr
+};
+
+}
