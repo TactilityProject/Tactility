21f14929b66ff963ff8af6a233727486ff6fe306
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2026-06-27 15:30:43 +0200

    Fix for startup issue

--- Devices/elecrow-crowpanel-basic-35/Source/Configuration.cpp
+++ Devices/elecrow-crowpanel-basic-35/Source/Configuration.cpp
@@ -6,7 +6,11 @@
 using namespace tt::hal;
 
 static bool initBoot() {
-    return driver::pwmbacklight::init(GPIO_NUM_27);
+    driver::pwmbacklight::init(GPIO_NUM_27);
+    // Delay is required, or GUI will lock up.
+    // It's possibly related to the increased power draw when turning on the backlight.
+    vTaskDelay(100);
+    return true;
 }
 
 static DeviceVector createDevices() {
