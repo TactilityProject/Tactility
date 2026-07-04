fd19928d07d7f681c53c9043354c40607da72934
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2026-07-05 00:12:31 +0200

    Remove build files

--- build-tests/Tests/crypt-module/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ken/Projects/Tactility/Tests/crypt-module
-# Build directory: /home/ken/Projects/Tactility/build-tests/Tests/crypt-module
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[CryptModuleTests]=] "/home/ken/Projects/Tactility/build-tests/Tests/crypt-module/CryptModuleTests")
-set_tests_properties([=[CryptModuleTests]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/ken/Projects/Tactility/Tests/crypt-module/CMakeLists.txt;12;add_test;/home/ken/Projects/Tactility/Tests/crypt-module/CMakeLists.txt;0;")
