2fb4bbb7c39c0b17fd1d8f909c32c45e517e386b
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2026-07-05 09:41:48 +0200

    Add symbols to crypt-module

--- Modules/crypt-module/source/module.cpp
+++ Modules/crypt-module/source/module.cpp
@@ -1,4 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
+#include <tactility/crypt.h>
+#include <tactility/hash.h>
 #include <tactility/module.h>
 
 extern "C" {
@@ -11,11 +13,21 @@ static error_t stop() {
     return ERROR_NONE;
 }
 
+static const ModuleSymbol crypt_module_symbols[] = {
+    DEFINE_MODULE_SYMBOL(crypt_get_iv),
+    DEFINE_MODULE_SYMBOL(crypt_generate_iv),
+    DEFINE_MODULE_SYMBOL(crypt_encrypt),
+    DEFINE_MODULE_SYMBOL(crypt_decrypt),
+    DEFINE_MODULE_SYMBOL(djb2_str),
+    DEFINE_MODULE_SYMBOL(djb2_data),
+    MODULE_SYMBOL_TERMINATOR
+};
+
 Module crypt_module = {
     .name = "crypt",
     .start = start,
     .stop = stop,
-    .symbols = nullptr,
+    .symbols = crypt_module_symbols,
     .internal = nullptr
 };
 
