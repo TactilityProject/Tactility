efe7febcaece2cf3815f826c3767e03cd5136088
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2026-07-12 15:32:41 +0200

    Update stickc-plus2 for dts drivers

--- Devices/m5stack-stickc-plus2/Source/module.cpp
+++ Devices/m5stack-stickc-plus2/Source/module.cpp
@@ -1,9 +1,32 @@
 #include <tactility/module.h>
+#include <tactility/error.h>
+
+#include <Tactility/hal/Configuration.h>
+
+#include <driver/gpio.h>
+
+constexpr auto* TAG = "StickCPlus2";
+
+// Legacy placeholder (required until legacy HAL is cleaned up everywhere)
+extern const tt::hal::Configuration hardwareConfiguration = {};
 
 extern "C" {
 
+static bool init_power() {
+    // CH552 applies 4 V to GPIO 0, which reduces Wi-Fi sensitivity.
+    // Setting output to high adds a bias of 3.3 V and suppresses over-voltage:
+    gpio_set_direction(GPIO_NUM_0, GPIO_MODE_OUTPUT);
+    gpio_set_level(GPIO_NUM_0, 1);
+
+    // "Hold power" pin: must be set to high to keep the device powered on:
+    gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT);
+    gpio_set_level(GPIO_NUM_4, 1);
+
+    return true;
+}
+
 static error_t start() {
-    // Empty for now
+    init_power();
     return ERROR_NONE;
 }
 
@@ -12,7 +35,7 @@ static error_t stop() {
     return ERROR_NONE;
 }
 
-struct Module m5stack_stickc_plus2_module = {
+Module m5stack_stickc_plus2_module = {
     .name = "m5stack-stickc-plus2",
     .start = start,
     .stop = stop,
