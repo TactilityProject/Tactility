419f19346d34805cf5b58dda9759c776d218d841
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2026-07-23 23:29:51 +0200

    Module cleanup

--- Devices/lilygo-tdeck-pro/source/module.cpp
+++ Devices/lilygo-tdeck-pro/source/module.cpp
@@ -2,22 +2,8 @@
 
 extern "C" {
 
-static error_t start() {
-    // Empty for now
-    return ERROR_NONE;
-}
-
-static error_t stop() {
-    // Empty for now
-    return ERROR_NONE;
-}
-
 struct Module lilygo_tdeck_pro_module = {
-    .name = "lilygo-tdeck-pro",
-    .start = start,
-    .stop = stop,
-    .symbols = nullptr,
-    .internal = nullptr
+    .name = "lilygo-tdeck-pro"
 };
 
 }
--- Drivers/gdeq031t10-module/source/module.cpp
+++ Drivers/gdeq031t10-module/source/module.cpp
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: Apache-2.0
-#include <tactility/check.h>
 #include <tactility/driver.h>
 #include <tactility/module.h>
 
@@ -7,26 +6,14 @@ extern Driver gdeq031t10_driver;
 
 extern "C" {
 
-static error_t start() {
-    /* We crash when construct fails, because if a single driver fails to construct,
-     * there is no guarantee that the previously constructed drivers can be destroyed */
-    check(driver_construct_add(&gdeq031t10_driver) == ERROR_NONE);
-    return ERROR_NONE;
-}
-
-static error_t stop() {
-    /* We crash when destruct fails, because if a single driver fails to destruct,
-     * there is no guarantee that the previously destroyed drivers can be recovered */
-    check(driver_remove_destruct(&gdeq031t10_driver) == ERROR_NONE);
-    return ERROR_NONE;
-}
+static Driver* const gdeq031t10_drivers[] = {
+    &gdeq031t10_driver,
+    nullptr
+};
 
 Module gdeq031t10_module = {
     .name = "gdeq031t10",
-    .start = start,
-    .stop = stop,
-    .symbols = nullptr,
-    .internal = nullptr
+    .drivers = gdeq031t10_drivers
 };
 
 } // extern "C"
