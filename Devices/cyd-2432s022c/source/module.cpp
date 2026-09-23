abdbd0f46f562600cf2decd6e53c65980534d012
NellowTCS <neeljaiswal23@gmail.com>
2026-09-23 12:57:28 -0600

    CYD 2.2 config

---
+++ Devices/cyd-2432s022c/source/module.cpp
@@ -0,0 +1,14 @@
+#include <tactility/module.h>
+
+extern "C" {
+
+Module cyd_2432s022c_module = {
+    .name = "cyd-2432s022c",
+    .start = nullptr,
+    .stop = nullptr,
+    .drivers = nullptr,
+    .symbols = nullptr,
+    .internal = nullptr
+};
+
+}
